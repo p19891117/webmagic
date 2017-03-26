3f8458be726ce354423f73a56d5bf8ffcf2b8909
smalljie <329126089@qq.com>
2017-03-26 23:46:52 +0800

    log

--- webmagic-core/src/main/java/us/codecraft/webmagic/pipeline/ConsolePipeline.java
+++ webmagic-core/src/main/java/us/codecraft/webmagic/pipeline/ConsolePipeline.java
@@ -1,9 +1,12 @@
 package us.codecraft.webmagic.pipeline;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
-
-import java.util.Map;
 
 /**
  * Write results in console.<br>
@@ -13,12 +16,12 @@ import java.util.Map;
  * @since 0.1.0
  */
 public class ConsolePipeline implements Pipeline {
-
+	private Logger logger = LoggerFactory.getLogger(ConsolePipeline.class); 
     @Override
     public void process(ResultItems resultItems, Task task) {
-        System.out.println("get page: " + resultItems.getRequest().getUrl());
+    	//logger.info("get page: " + resultItems.getRequest().getUrl());
         for (Map.Entry<String, Object> entry : resultItems.getAll().entrySet()) {
-            System.out.println(entry.getKey() + ":\t" + entry.getValue());
+        	logger.info(entry.getKey() + ":\t" + entry.getValue());
         }
     }
 }
--- webmagic-core/src/test/java/us/codecraft/webmagic/SpiderTest.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/SpiderTest.java
@@ -1,10 +1,14 @@
 package us.codecraft.webmagic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import us.codecraft.webmagic.exception.PageProcessException;
 import us.codecraft.webmagic.processor.PageProcessor;
 import us.codecraft.webmagic.processor.URLProcessAbs;
 
 public class SpiderTest {
+	private Logger logger = LoggerFactory.getLogger(SpiderTest.class); 
 	public static void main(String[] args) {
 		Request req = Request.custom().setUrl("https://www.baidu.com/").build();
 		Spider spider = Spider.custom().addRequests(req)
@@ -19,6 +23,6 @@ class Baidu extends URLProcessAbs{
 	}
 	@Override
 	protected void processPage(Task task, PageProcessor pageProcessor, Page page) throws PageProcessException {
-		System.out.println(page.getRawText());
+		logger.info(page.getRawText());
 	}
 }
